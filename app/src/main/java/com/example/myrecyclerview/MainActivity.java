c2e2774b4101d8730c00efaa382c8e2c956d69d6
fahmiw <fahmi.widianto.tif18@polban.ac.id>
2020-04-07 21:17:46 +0700

    RecycleView Mode CardView Finish

--- app/src/main/java/com/example/myrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/myrecyclerview/MainActivity.java
@@ -14,12 +14,14 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView rvHeroes;
+    private String title = "Mode List";
     private final ArrayList<Hero> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setActionBarTitle(title);
 
         rvHeroes = findViewById(R.id.rv_heroes);
         rvHeroes.setHasFixedSize(true);
@@ -48,18 +50,34 @@ public class MainActivity extends AppCompatActivity {
     private void setMode(int itemId) {
         switch (itemId){
             case R.id.action_list:
+                title = "Mode List";
                 showRecyclerList();
                 break;
             case R.id.action_grid:
+                title = "Mode Grid";
                 showRecyclerGrid();
                 break;
             case R.id.action_cardview:
+                title = "Mode CardView";
+                showRecyclerCardView();
                 break;
         }
+        setActionBarTitle(title);
     }
     private void showRecyclerGrid(){
         rvHeroes.setLayoutManager(new GridLayoutManager(this, 2));
         GridHeroAdapter gridHeroAdapter = new GridHeroAdapter(list);
         rvHeroes.setAdapter(gridHeroAdapter);
+    }
+
+    private void showRecyclerCardView(){
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        CardViewHeroAdapter cardViewHeroAdapter = new CardViewHeroAdapter(list);
+        rvHeroes.setAdapter(cardViewHeroAdapter);
+    }
+    private void setActionBarTitle(String title){
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setTitle(title);
+        }
     }
 }
