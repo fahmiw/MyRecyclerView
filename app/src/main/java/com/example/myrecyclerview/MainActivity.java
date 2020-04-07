e93a2b35d7b064e469fa1557533e613ea5fa2edf
fahmiw <fahmi.widianto.tif18@polban.ac.id>
2020-04-07 19:56:54 +0700

    RecycleView Mode List Finish

--- app/src/main/java/com/example/myrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/myrecyclerview/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.myrecyclerview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvHeroes;
+    private final ArrayList<Hero> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvHeroes = findViewById(R.id.rv_heroes);
+        rvHeroes.setHasFixedSize(true);
+
+        list.addAll(HeroesData.getListData());
+        showRecyclerList();
+    }
+
+    private void showRecyclerList() {
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        ListHeroAdapter listHeroAdapter = new ListHeroAdapter(list);
+        rvHeroes.setAdapter(listHeroAdapter);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        setMode(item.getItemId());
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setMode(int itemId) {
+        switch (itemId){
+            case R.id.action_list:
+
+            case R.id.action_grid:
+
+            case R.id.action_cardview:
+                break;
+        }
     }
 }
